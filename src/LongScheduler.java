6a64237ad9e83d8044ddd7d781f2d3ae6d2150ec
amnewson <aaronmn470@gmail.com>
2021-11-04 16:15:23 -0400

    Work for the Schedulers

--- src/LongScheduler.java
+++ src/LongScheduler.java
@@ -1,2 +1,30 @@
-public class LongScheduler {
+import java.util.Queue;
+ class LongScheduler {
+    String processState = "";
+    boolean IO; //placeholder boolean for IO checks
+    int process;
+    Queue<Integer>ready;  //placeholder ready queue
+    void StateChange() {  //change the state of the process
+        switch (processState) {
+            case ("running"):
+            {
+                if(IO == true) {
+                    processState = "waiting"; // for a process waiting on IO operations
+                } else {
+                    processState = "ready";   //why would it go to ready?
+                }
+                break;
+            }
+            case ("waiting"):
+            {
+                if(IO == false) {
+                    processState = "ready"; //completed the interruption or IO operation
+                }
+            }
+            default:
+            {
+                processState = "terminate";
+            }
+        }
+    }
 }
--- src/ShortScheduler.java
+++ src/ShortScheduler.java
@@ -1,2 +1,15 @@
+import java.util.Queue;
+
+//11/4/21
 public class ShortScheduler {
+    int readyProcess; //the process with the ready state
+    int ramSpace; //placeholder for RAM
+    void selection(Queue<Integer> q1) // select the first process with the ready state
+    {
+        for (int m = 0; m < q1.size(); m++)
+        {
+            readyProcess = q1.remove(); //take the first ready process out of the queue
+            ramSpace = readyProcess; // place it into the RAM
+        }
+    }
 }

a230ef746f9dc91c19bb4e3a5f34b6988a453c18
amnewson <aaronmn470@gmail.com>
2021-11-05 16:18:36 -0400

    Long Scheduler Changes

--- src/LongScheduler.java
+++ src/LongScheduler.java
@@ -3,27 +3,41 @@ import java.util.Queue;
     String processState = "";
     boolean IO; //placeholder boolean for IO checks
     int process;
-    Queue<Integer>ready;  //placeholder ready queue
+    Queue<Integer>ready;    //placeholder ready process queue
+     Queue<Integer>wait;    //placeholder waiting process queue
+     Queue<Integer>start;    //placeholder new process queue
+
     void StateChange() {  //change the state of the process
         switch (processState) {
+            //add() adds the process to the specific queue based on the process current state
             case ("running"):
             {
-                if(IO == true) {
+                if(IO) {
                     processState = "waiting"; // for a process waiting on IO operations
+                    wait.add(process);
                 } else {
-                    processState = "ready";   //why would it go to ready?
+                    processState = "ready";   //why would it go to ready? Interrupts
+                    ready.add(process);
                 }
                 break;
             }
             case ("waiting"):
             {
-                if(IO == false) {
+                if(!IO) {
                     processState = "ready"; //completed the interruption or IO operation
+                    ready.add(process);
                 }
+                break;
+            }
+            case ("new"):
+            {
+                start.add(process);   // for the processes that just entered the RAM
+                break;
             }
             default:
             {
                 processState = "terminate";
+                break;
             }
         }
     }
